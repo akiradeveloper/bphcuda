bc596b906e4ea0f870a162e0241091f049c2bec4
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-12 15:30:26 +0900

    modified project.rb

--- project.rb
+++ project.rb
@@ -1,13 +1,13 @@
 # This path should be changed by client
 THRUST_HOME = "#{ENV["HOME"]}/local/thrust"
-
-THRUST_
-THRUST_TESTLIB = [THRUST_HOME, "testing"
+THRUST_LIB = THRUST_HOME
 
 thisdir = File.dirname(__FILE__)
-LIBPATH = [thisdir, "bphcuda"].join "/"
+LIBPATH = thisdir
 VERSION = "0.0.0"
 DEVELOPER = ["Akira Hayakawa <ruby.wktk@gmail.com>"]
 TESTPATH = 
 COMPILER = "nvcc"
 CC = [COMPILER, LIBPATH, THRUST_LIB].join " -I "
+
+p CC
