0cdde3d19b964754a686bdd91c2e8a382a2eb871
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-14 18:31:42 +0900

    testbin is .bin suffix

--- test/compile.rb
+++ test/compile.rb
@@ -14,7 +14,7 @@ if __FILE__ == $0
   src = ARGV[0]
   if File.extname(src) == ".cu"
     # ruby compile.rb hoge.cu
-    bin = File.basename(src, ".cu")
+    bin = File.basename(src, ".cu") + ".bin"
     compile(src, bin) 
     run(bin)
   end
