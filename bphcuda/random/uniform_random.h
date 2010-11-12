ec93891aba05de018a909256fb18025ebae6b3f5
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-12 17:00:51 +0900

    tested uniform_rand

--- bphcuda/random/uniform_random.h
+++ bphcuda/random/uniform_random.h
@@ -13,37 +13,36 @@ namespace {
 
 namespace bphcuda {
 
-namespace {
-struct uniform_rand_generator :public thrust::unary_function<size_t, real> {
+struct uniform_random_generator :public thrust::unary_function<size_t, real> {
   real2 _range;
   size_t _seed;
-  shell_rand_adapter(real2 range, size_t seed)
+  uniform_random_generator(real2 range, size_t seed)
   :_range(range), _seed(seed){}
   __host__ __device__
   real operator()(size_t idx) const {
     thrust::default_random_engine rng(_seed);
     const size_t skip = 1;
     rng.discard(skip * idx);
-    thrust::uniform_real_distribution<real> u_lower_upper(range.get<0>(), range.get<1>());
+    thrust::uniform_real_distribution<real> u_lower_upper(_range.get<0>(), _range.get<1>());
     return u_lower_upper(rng); 	 
   }
 };
-} // END namespace 
 
 // impl but not tested
 /*
   generate uniform random number [lower, upper] to array
 */
-template<typaname RealIterator>
-alloc_uniform_random(
-  size_t n_particles,
+template<typename RealIterator>
+void alloc_uniform_random(
+  size_t n_particle,
   RealIterator begin,
   thrusting::real2 range,
   size_t seed
 ){
+  size_t origin = 0;
   thrust::transform(
-    thrust::counting_iterator<size_t>(1),
-    thrust::counting_iterator<size_t>(n_particle + 1),
+    thrust::counting_iterator<size_t>(origin),
+    thrust::counting_iterator<size_t>(origin + n_particle),
     begin,
     uniform_random_generator(range, seed));
 }
--- testing/gtest/uniform_random.cu
+++ testing/gtest/uniform_random.cu
@@ -1,8 +1,25 @@
 #include <thrusting/vector.h>
+#include <thrusting/list.h>
 
 #include <bphcuda/random/uniform_random.h>
 
+#include <iostream>
+
 #include <gtest/gtest.h>
 
-TEST(uniform_random, test1){
+namespace {
+  using thrusting::real;
+  using thrusting::real2;
+}
+
+TEST(uniform_random, printout){
+  size_t count = 10;
+  THRUSTING_VECTOR<real> xs(count);
+  size_t seed = 0;
+  bphcuda::alloc_uniform_random(
+    count,
+    xs.begin(),
+    real2(0.1, 0.5),
+    seed);
+  std::cout << thrusting::make_list(xs) << std::endl;
 }
