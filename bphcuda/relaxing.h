0b83095a54d97c7574e3546df764e2b60538b02c
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-16 20:54:26 +0900

    impl relax_all but not tested

--- bphcuda/relaxing.h
+++ bphcuda/relaxing.h
@@ -131,8 +131,8 @@ void alloc_new_c_all(
   I1 idx, // input
   RealTmp tmp1, RealTmp tmp2, RealTmp tmp3, // output
   size_t n_cell, 
-  IntTmp tmp4,
   RealTmp tmp5, RealTmp tmp6, RealTmp tmp7,
+  IntTmp tmp4,
   size_t seed
 ){
   // alloc new velocity all over the particles
@@ -140,6 +140,7 @@ void alloc_new_c_all(
     n_particle,
     u, v, w,
     seed);
+
   // calculate the average velocities in each cell
   thrust::reduce_by_key(
     idx,
@@ -147,6 +148,7 @@ void alloc_new_c_all(
     thrusting::make_zip_iterator(u, v, w),
     tmp4, // cell count
     thrusting::make_zip_iterator(tmp5, tmp6, tmp7)); // sum velocity
+
   // averaging by cell count
   thrust::transform(
     thrusting::make_zip_iterator(tmp5, tmp6, tmp7),
@@ -154,12 +156,14 @@ void alloc_new_c_all(
     tmp4,
     thrusting::make_zip_iterator(tmp5, tmp6, tmp7), // average velocity in cell
     thrusting::divides<real3, size_t>()); 
+
   // create n_particle length of average velocity in each cell
   thrust::gather(
     idx,
     thrusting::advance(n_particle, idx),
     thrusting::make_zip_iterator(tmp5, tmp6, tmp7),
     thrusting::make_zip_iterator(tmp1, tmp2, tmp3)); // the average velocity of n_particle length
+
   // modified velocity
   thrust::transform(
     thrusting::make_zip_iterator(u, v, w),
@@ -169,13 +173,91 @@ void alloc_new_c_all(
     thrust::minus<real3>());
 }
 
+struct pow_e :public thrust::unary_function<real3, real> {
+  real operator()(const real3 &in) const {
+    real x = in.get<0>();
+    real y = in.get<1>();
+    real z = in.get<2>();
+    return x*x + y*y + z*z;
+  }
+};
+
+struct sqrt :public thrust::unary_function<real, real> {
+  real operator()(real x) const {
+    return sqrt(x);
+  }
+};
+
 template<typename R1, typename R2>
 void relax_all(
   size_t n_particle,
   R1 u, R1 v, R1 w,
   I1 idx,
-  R3 tmp1, R3 tmp2, R3 tmp3,
+  RealTmp tmp1, RealTmp tmp2, RealTmp tmp3,
   size_t n_cell,
-  R3 tmp4, R3 tmp5, R3 tmp6,
+  RealTmp tmp4, 
+  RealTmp tmp5, RealTmp tmp6, RealTmp tmp7,
+  IntTmp tmp8,
+  size_t seed
+){
+  // calc_total_e ahead of allocating new velocity
+  thrust::reduce_by_key(
+    idx,
+    thrusting::advance(n_particle, idx),
+    thrust::make_transform_iterator(
+      thrusting::make_zip_iterator(u, v, w),
+      pow_e()),
+    tmp8, // cell count
+    tmp4); // old energy
 
+  // allocate new velocity preserving the momentum in each cell
+  alloc_new_c_all(
+    n_particle,
+    u, v, w,
+    idx,
+    tmp1, tmp2, tmp3,
+    n_cell,
+    tmp5, tmp6, tmp7,
+    seed);
+
+  // again, calc_e_total_e
+  thrust::reduce_by_key(
+    idx,
+    thrusting::advance(n_particle, idx),
+    thrust::make_transform_iterator(
+      thrusting::make_zip_iterator(u, v, w),
+      pow_e()),
+    tmp8, // cell count, again
+    tmp5); // new energy. reusing tmp5
+
+  // the ratio_e = old / new
+  thrust::transform(
+    tmp4,
+    thrusting::advance(n_cell, tmp4),
+    tmp5,
+    tmp4,
+    thrust::divides<real>());
+
+  // sqrt it
+  thrust::transform(
+    tmp4,
+    thrusting::advance(n_cell, tmp4),
+    tmp4,
+    sqrt());  
+
+  // the ratio_e all over the particle 
+  thrust::gather(
+    idx,
+    thrusting::advance(n_particle, idx),
+    tmp4,
+    tmp1); // reusing tmp1, sqrt of ratio_e
+
+  // modified the velocity by ratio_e
+  thrust::transform(
+    tmp1,
+    thrusting::advance(n_particle, tmp1),
+    thrusting::make_zip_iterator(u, v, w),
+    thrusting::make_zip_iterator(u, v, w),
+    thrusting::multiplies<real, real3>());   
+}
 } // END bphcuda
