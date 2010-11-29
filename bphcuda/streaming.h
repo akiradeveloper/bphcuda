eecd272051af07f3ba372be1d628f73505b6d0f1
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-29 15:32:06 +0900

    passed simple tests for runge kutta method of 1, 2 steps

--- bphcuda/streaming.h
+++ bphcuda/streaming.h
@@ -3,6 +3,7 @@
 #include <thrusting/real.h>
 #include <thrusting/vectorspace.h>
 #include <thrusting/tuple.h>
+#include <thrusting/functional.h>
 
 /*
   moving functions are type
@@ -16,17 +17,19 @@ namespace {
 namespace bphcuda {
 
 namespace detail {
-real6 make_real6(real3 a, real3 b){
+__host__ __device__
+real6 make_real6(const real3 &a, const real3 &b){
   return real6(
     a.get<0>(), a.get<1>(), a.get<2>(),
-    a.get<3>(), a.get<4>(), a.get<5>());
+    b.get<0>(), b.get<1>(), b.get<2>());
 }
 
-real7 make_real7(real6 a, real){
+__host__ __device__
+real7 make_real7(const real6 &a, const real &b){
   return real7(
     a.get<0>(), a.get<1>(), a.get<2>(),
     a.get<3>(), a.get<4>(), a.get<5>(),
-    a.get<6>());
+    b);
 }
 } // END detail
 
@@ -35,17 +38,17 @@ namespace detail {
   1 step Runge Kutta.
 */
 template<typename ForceGenerator>
-struct _runge_kutta_1 :public thrust::unary_function<real7, real6> {
+struct runge_kutta_1 :public thrust::unary_function<real7, real6> {
   ForceGenerator _f;
   real _dt;
-  _move(ForceGenerator f, real dt)
+  runge_kutta_1(ForceGenerator f, real dt)
   :_f(f), _dt(dt){}
   __device__ __host__
   real6 operator()(const real7 &in){
     real3 p = real3(in.get<0>(), in.get<1>(), in.get<2>());
     real3 c = real3(in.get<3>(), in.get<4>(), in.get<5>());
     real m = in.get<6>();
-    real3 force = _f(in)
+    real3 force = _f(in);
     real3 a = force / m;
     real3 new_p = p + _dt * c; // is this correct without 0.5 * m * a^2 ? 
     real3 new_c = c + _dt * a;
@@ -55,34 +58,39 @@ struct _runge_kutta_1 :public thrust::unary_function<real7, real6> {
 } // END detail
 
 template<typename ForceGenerator>
-_runge_kutta_1<ForceGenerator> runge_kutta_1(ForceGenerator f, real dt){
-  return _runge_kutta_1<ForceGenerator>(f, dt);
+detail::runge_kutta_1<ForceGenerator> runge_kutta_1(ForceGenerator f, real dt){
+  return detail::runge_kutta_1<ForceGenerator>(f, dt);
 }
 
 namespace detail {
+
 /*
   2 step Runge Kutta.
 */
 template<typename ForceGenerator>
-struct _runge_kutta_2 :public thrust::unary_function<real7, real6> {
+struct runge_kutta_2 :public thrust::unary_function<real7, real6> {
   ForceGenerator _f;
   real _dt;
-  _move(ForceGenerator f, real dt)
+  runge_kutta_2(ForceGenerator f, real dt)
   :_f(f), _dt(dt){}
   __device__ __host__
   real6 operator()(const real7 &in){
-    /*
-      h_ prefix means the value is at _dt/2 time passed.
-    */
-    // calc result by half time.
-    real6 h_result = runge_kutta_1(_f, 0.5 * _dt)(in);
-    // calc state of particle at the time half.
-    real7 h_state = make_real7(h_result, m);
-    real3 h_c = real3(h_state.get<3>(), h_state.get<4>(), h_state<5>());
-    real3 h_force = _f(h_state);
-    real3 h_a = h_force / m;
     real3 p = real3(in.get<0>(), in.get<1>(), in.get<2>()); 
     real3 c = real3(in.get<3>(), in.get<4>(), in.get<5>());
+    real m = in.get<6>();
+    /*
+      calc result at half time.
+    */
+    real6 h_result = bphcuda::runge_kutta_1(_f, 0.5 * _dt)(in);
+    /*
+      produce state of particle at the time half.
+    */
+    real7 h_state = make_real7(h_result, m);
+    /*
+      calc c and a at half time.
+    */
+    real3 h_c = real3(h_state.get<3>(), h_state.get<4>(), h_state.get<5>());
+    real3 h_a = _f(h_state) / m;
     real3 new_p = p + _dt * h_c; // is this correct ? 
     real3 new_c = c + _dt * h_a;
     return detail::make_real6(new_p, new_c);
@@ -91,8 +99,8 @@ struct _runge_kutta_2 :public thrust::unary_function<real7, real6> {
 } // END detail
 
 template<typename ForceGenerator>
-_runge_kutta_2<ForceGenerator> runge_kutta_2(ForceGenerator f, real dt){
-  return _runge_kutta_2<ForceGenerator>(f, dt);
+detail::runge_kutta_2<ForceGenerator> runge_kutta_2(ForceGenerator f, real dt){
+  return detail::runge_kutta_2<ForceGenerator>(f, dt);
 }
 
 } // END bphcuda
--- testing/gtest/streaming.h
+++ testing/gtest/streaming.h
@@ -1,6 +1,7 @@
 #include <bphcuda/streaming.h>
 
 #include <thrusting/real.h>
+#include <thrusting/functional.h>
 
 #include <gtest/gtest.h>
 
@@ -8,8 +9,50 @@ namespace {
   using namespace thrusting;
 }
 
-TEST(Streaming, RungeKutta1){
+TEST(Streaming, Detail){
+  EXPECT_EQ(real6(1,2,3,4,5,6), bphcuda::detail::make_real6(real3(1,2,3), real3(4,5,6)));
+  EXPECT_EQ(real7(1,2,3,4,5,6,7), bphcuda::detail::make_real7(real6(1,2,3,4,5,6), 7));
 }
 
-TEST(Streaming, RungeKutta2){
+TEST(RungeKutta1, ConstantForce){
+  real7 before = real7(
+    0,0,0,
+    1,0,0,
+    1); 
+  
+  real6 after = bphcuda::runge_kutta_1(
+    thrusting::constant(real3(-1,0,0)), 0.2)(before);
+    
+   
+  real3 pos = real3(after.get<0>(), after.get<1>(), after.get<2>());
+  real3 vel = real3(after.get<3>(), after.get<4>(), after.get<5>());
+
+  EXPECT_EQ(
+    real3(0.2,0,0),
+    pos);
+
+  EXPECT_EQ(
+    real3(0.8,0,0),
+    vel);
+}
+
+TEST(RungeKutta2, ConstantForce){
+  real7 before = real7(
+    0,0,0,
+    1,0,0,
+    1); 
+  
+  real6 after = bphcuda::runge_kutta_2(
+    thrusting::constant(real3(-1,0,0)), 0.2)(before);
+    
+  real3 pos = real3(after.get<0>(), after.get<1>(), after.get<2>());
+  real3 vel = real3(after.get<3>(), after.get<4>(), after.get<5>());
+
+  EXPECT_EQ(
+    real3(0.18,0,0),
+    pos);
+
+  EXPECT_EQ(
+    real3(0.8,0,0),
+    vel);
 }
